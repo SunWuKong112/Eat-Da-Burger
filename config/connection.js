9e477cdec8b5f2c99169f237f9273e8f733f818f
Kyle Gribble <dribbleGribble0516@gmail.com>
2020-09-11 22:01:00 -0500

    Set up connection with jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-     host: "localhost",
-     port: 3306,
-     user: "root",
-     password: "Doodle.bomb.com0516@",
-     database: "burgers_db"
-});
+if(process.env.JAWSDB_URL)
+{
+     conse connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else
+{
+     const connection = mysql.createConnection({
+          host: "localhost",
+          port: 3306,
+          user: "root",
+          password: "Doodle.bomb.com0516@",
+          database: "burgers_db"
+     });
+}
    
 connection.connect(function(err) {
      if (err) {
